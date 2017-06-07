c6b66e56a16aab46d2a915b85f92b4075dfa7101
BLUEHOLE\janakuma <janakuma@bluehole.net>
2017-06-07 20:03:12 +0900

    두번째 수정

--- template/src/App.js
+++ template/src/App.js
@@ -10,6 +10,12 @@ class App extends Component {
 					<img src={logo} className="App-logo" alt="logo" />
 					<h2>Welcome to React</h2>
 				</div>
+
+				<p>
+					배틀그라운드 배너 영역
+				</p>
+
+
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
 				</p>
